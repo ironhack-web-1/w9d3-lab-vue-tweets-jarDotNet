f4bd4ec2f914651d6204f954ac2236424ad35419
José Antonio Reyes <joseantonio@yukisoftware.es>
2022-09-03 11:53:36 +0200

    done

---
+++ src/components/Timestamp.vue
@@ -0,0 +1,18 @@
+<template>
+  <span className="timestamp">{{ time }}</span>
+</template>
+
+<script>
+export default {
+  name: "Timestamp",
+  props: {
+    time: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,21 @@
+<template>
+  <span className="user" v-if="userData">
+    <span className="name">{{ userData.name }}</span>
+    <span className="handle">@{{ userData.handle }}</span>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "User",
+  props: {
+    userData: {
+      type: Object,
+      default: null
+    }
+  },
+};
+</script>
+
+<style>
+</style>
